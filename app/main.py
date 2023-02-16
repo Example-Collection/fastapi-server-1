27728194e7d8dfc69426b6970e5c671343478c2b
sang-w0o <robbyra@gmail.com>
2023-02-16 17:00:25 +0900

    feat: Configure log format for Grafana

--- app/main.py
+++ app/main.py
@@ -1,18 +1,27 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from app.item import Item
 from app.service import call_server_2
 from app.otel import tracer
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+
 import logging
 
 app = FastAPI()
 
+LoggingInstrumentor().instrument(set_logging_format=True,
+                                 logging_format=' trace_id=%(otelTraceID)s span_id=%(otelSpanID)s - %(message)s')
+
 
 @app.post("/items", status_code=201)
-async def server_1_handler(item: Item):
-    with tracer.start_as_current_span("server-1-handler"):
+async def server_1_handler(item: Item, request: Request):
+    with tracer.start_as_current_span("server-1-handler") as span:
+        span.set_attribute("item_name", item.name)
+        span.set_attribute("item_price", item.price)
+        span.set_attribute("ip", request.client.host)
+        span.set_attribute("path", request.url.path)
         logging.warning(f"[SERVER-1] Received item: {item}")
         result = call_server_2(item)
         if result.status_code != 200:
