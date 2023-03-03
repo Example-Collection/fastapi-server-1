15c8e322f285bd01ca9bfd776e949cc7e13f1d60
sang-w0o <robbyra@gmail.com>
2023-03-03 13:57:32 +0900

    feat: Use traceparent header to propagate context

--- app/main.py
+++ app/main.py
@@ -6,9 +6,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.item import Item
 from app.service import call_server_2
 from app.otel import tracer
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 
 import logging
+
+
+def get_trace_parent_header(request: Request):
+    return request.headers.get("traceparent")
 
 
 app = FastAPI()
@@ -30,7 +35,10 @@ LoggingInstrumentor().instrument(set_logging_format=True,
 
 @app.post("/items", status_code=201)
 async def server_1_handler(item: Item, request: Request):
-    with tracer.start_as_current_span("server-1-handler") as span:
+    traceparent = get_trace_parent_header(request)
+    carrier = {"traceparent": traceparent}
+    ctx = TraceContextTextMapPropagator().extract(carrier)
+    with tracer.start_as_current_span("server-1-handler", context=ctx) as span:
         span.set_attribute("item_name", item.name)
         span.set_attribute("item_price", item.price)
         span.set_attribute("ip", request.client.host)
